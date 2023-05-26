ad487fa6d382a4230778b41862f1a37ecc5457e7
Jeff Taff <jeff.hoarau@gmail.com>
2023-05-26 16:52:18 +0200

    ajout d'une liste d'entrée et affiche sa position sur une autre pafe

--- app/src/main/java/com/example/myapplication/SecondActivity.kt
+++ app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -3,10 +3,14 @@ package com.example.myapplication
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import com.example.myapplication.databinding.SecondActivityBinding
+
+class Counter(var c: Int = 0)
 
 class SecondActivity : Activity() {
     private lateinit var binding: SecondActivityBinding
+    private var counter = Counter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,5 +25,24 @@ class SecondActivity : Activity() {
             val i = Intent(this, MainActivity::class.java)
             startActivity(i)
         }
+
+        binding.button2.setOnClickListener {
+            counter.c + 1
+            binding.counter.text = counter.toString()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("counter", counter.c)
+    }
+
+    override fun onRestoreInstanceState(
+        savedInstanceState: Bundle?,
+        persistentState: PersistableBundle?
+    ) {
+        super.onRestoreInstanceState(savedInstanceState, persistentState)
+        counter = savedInstanceState.getInt("counter")
+        binding.counter.text = counter.toString()
     }
 }
